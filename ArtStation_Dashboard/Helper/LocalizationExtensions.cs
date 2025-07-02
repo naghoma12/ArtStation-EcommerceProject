9de0410cac95cf0caa3c7b5df453422b0d0d9cd6
naghoma12 <naghamy473@gmail.com>
2025-07-02 23:41:08 +0300

    Handle language

---
+++ ArtStation_Dashboard/Helper/LocalizationExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace ArtStation_Dashboard.Helper
+{
+    public static class LocalizationExtensions
+    {
+        public static string Localize(this (string Ar, string En) pair, string language)
+        {
+            return language == "en" ? pair.En : pair.Ar;
+        }
+    }
+}
