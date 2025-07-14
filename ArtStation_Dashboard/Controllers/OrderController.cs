87819b8af18cbc2fe060b7bc59e31203106d1983
ArwaAlaa1 <arwaalaa24682468@gmial.com>
2025-07-15 01:22:36 +0300

    Add UserController / Handle Banners Images

---
+++ ArtStation_Dashboard/Controllers/OrderController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ArtStation_Dashboard.Controllers
+{
+    public class OrderController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ ArtStation_Dashboard/ViewModels/User/UserViewModel.cs
@@ -0,0 +1,20 @@
+﻿using ArtStation.Core.Entities.Identity;
+using ArtStation.Core.Entities;
+
+namespace ArtStation_Dashboard.ViewModels.User
+{
+    public class UserViewModel
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public string? Country { get; set; }
+        public string? Email { get; set; }
+        public Gender? Gender { get; set; }
+        public DateOnly? BirthDay { get; set; }
+        public string? Nationality { get; set; }
+        public string? Image { get; set; }
+        public string PhoneNumber { get; set; }
+        public bool IsActive { get; set; } = true;
+     
+    }
+}
