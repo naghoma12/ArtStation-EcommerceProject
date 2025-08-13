e574befca8a3f101ae732f11c7194ca831c99e75
ArwaAlaa1 <arwaalaa24682468@gmial.com>
2025-08-13 20:02:17 +0300

    Add Pagination In bannner view

--- ArtStation.Core/Helper/PagedResult.cs
+++ ArtStation.Core/Helper/PagedResult.cs
@@ -6,7 +6,7 @@
         public int TotalItems { get; set; }
         public int PageNumber { get; set; }
         public int PageSize { get; set; }
-        public int? TotalPages {  get; set; }
+        public int? TotalPages { get; set; }
 
     }
 }
